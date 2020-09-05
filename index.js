ffb9b34bcb98e2c61bd66ba1f815a1025bec9b4c
Sapphirix Client <60874772+ChaotiqueSlime@users.noreply.github.com>
2020-09-05 12:23:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const Canvas = require('canvas');
 
 //This Is The Welcome Message
 
-client.on(`guildMemberAdd`, member =>{
-      const channel = member.guild.channels.cache.find(channel => channel.name === "welcome-leaves");
-      if(!channel) return;
-      const embed = new Discord.MessageEmbed()
-      .setDescription(`Welcome **${member.user.tag}**, to **${member.guild.name}**!`)
-      .setThumbnail(member.user.displayAvatarURL())
-      .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
-      .setTimestamp()
-      .setColor("#ff9dbb")
-      channel.send(embed)
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'welcome-leaves');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	ctx.strokeStyle = '#74037b';
+	ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+	ctx.beginPath();
+	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.clip();
+
+	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
+	ctx.drawImage(avatar, 25, 25, 200, 200);
+
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
 //This Is The Member Left Message didnt make this one into a embed but you can if you want
